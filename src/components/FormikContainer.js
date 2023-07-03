f62b893f4c8e7c56b2325d72883af21e70c23542
only1tej <tijanienny3@gmail.com>
2023-07-03 10:51:59 -0700

    Update

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Formik, Form } from "formik";
+import FormikControl from "./FormikControl";
 import * as Yup from "yup";
 
 function FormContainer() {
-  const initialValues = {};
-  const validationSchema = Yup.object({});
+  const initialValues = {
+    email: "",
+    description: "",
+  };
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Required"),
+    description: Yup.string().required("Required"),
+  });
   const onSubmit = (values) => console.log("Form data", values);
   return (
     <div>
@@ -15,6 +22,18 @@ function FormContainer() {
       >
         {(formik) => (
           <Form>
+            <FormikControl
+              control="input"
+              type="email"
+              label="Email"
+              name="email"
+            />
+            <FormikControl
+              control="textarea"
+              // type="textarea"
+              label="Description"
+              name="description"
+            />
             <button type="submit">Submit</button>
           </Form>
         )}
